33722608dfa57943540512e2ab03f9e372c55d34
Rafael Mindicelo <rmindicelo@gmail.com>
2021-03-12 16:58:09 -0300

    Criado migrations e models de: address, cities, countries e states

---
+++ src/database/migrations/20210312193735-create-cities.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('cities', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      city_name: {
+        type: Sequelize.STRING
+      },
+      state_id: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('cities');
+  }
+};
---
+++ src/database/migrations/20210312194225-create-address.js
@@ -0,0 +1,48 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('addresses', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        type: Sequelize.INTEGER
+      },
+      address_description: {
+        type: Sequelize.STRING
+      },
+      complemento: {
+        type: Sequelize.STRING
+      },
+      zip_code: {
+        type: Sequelize.STRING
+      },
+      neighboorhood: {
+        type: Sequelize.STRING
+      },
+      city_id: {
+        type: Sequelize.INTEGER
+      },
+      state_id: {
+        type: Sequelize.INTEGER
+      },
+      country_id: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('addresses');
+  }
+};
