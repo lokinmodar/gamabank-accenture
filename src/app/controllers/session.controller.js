f11e774cdfa442420de5bc94cd0367fd541d6c64
Dante Souza e Souza <lokinmodar@gmail.com>
2021-03-13 21:41:20 -0300

    Refatorado Session Controller

--- src/app/controllers/session.controller.js
+++ src/app/controllers/session.controller.js
@@ -2,15 +2,14 @@ import jwt from 'jsonwebtoken';
 import * as Yup from 'yup';
 
 import User from '../models/user.model';
+import Sessions from '../models/sessions.model';
 import authConfig from '../../configs/auth'; // gerar arquivo exportando chave secreta e data de expiração
 
 class SessionController {
   async store(req, res) {
     const schema = Yup.object().shape({
-      user_email: Yup.string()
-        .email()
-        .required(),
-      password: Yup.string().required()
+      user_email: Yup.string().email().required(),
+      password: Yup.string().required(),
     });
 
     if (!(await schema.isValid(req.body))) {
@@ -33,18 +32,16 @@ class SessionController {
       return res.status(401).json({ error: 'Incorrect password.' });
     }
 
-    const { id, name } = user;
+    //
+    const { user_id } = user;
+    const token = await jwt.sign({ user_id }, authConfig.secret, {
+      expiresIn: authConfig.expiresIn,
+    });
+
+    const createdSession = await Sessions.create({ user_id, token });
 
     return res.json({
-      user: {
-        id,
-        name,
-        user_email,
-      },
-      token: jwt.sign({ id }, authConfig.secret, {
-        expiresIn: authConfig.expiresIn,
-      }), // enviando Payload (informações que serão incorporadas ao token,
-      // chave única do app)
+      createdSession,
     });
   }
 }
