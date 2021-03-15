a47d5254b1e6ad233b6e55377c35a6bd9e8e3c13
Dante Souza e Souza <lokinmodar@gmail.com>
2021-03-15 12:37:48 -0300

    Ajustado padrão de código no helper de transactions

--- src/helpers/transactionFunctions.helper.js
+++ src/helpers/transactionFunctions.helper.js
@@ -1,48 +1,48 @@
-let cliente = {
-    nome: 'diego',
-    conta: '0025-2',
-    saldo: 0
-}
+const cliente = {
+  nome: 'diego',
+  conta: '0025-2',
+  saldo: 0,
+};
 
-let cliente2 = {
-    nome: 'mario',
-    conta: '0025-1',
-    saldo: 1000
-}
+const cliente2 = {
+  nome: 'mario',
+  conta: '0025-1',
+  saldo: 1000,
+};
 
 function sacar(valor) {
-    this.saldo = 1000    
-    if(saldo < valor){
-    console.log(`${valor} excede o limite disponível.`)
-}
-}
-
-let deposito = function(valorDepositado) {
-    if(valorDepositado <= 0) {
-        console.log("O valor depositado tem que ser maior que zero.")
-    } else {
-        cliente.saldo += valorDepositado
-        console.log(cliente.saldo)
-    }
-    
+  this.saldo = 1000;
+  if (saldo < valor) {
+    console.log(`${valor} excede o limite disponível.`);
+  }
 }
 
-let debito = function(valor) {
-    if(valor > cliente.saldo){
-        console.log("Saldo insuficiente.")
-    } else {
-        cliente.saldo -= valor
-        console.log(cliente.saldo)
-    }
-}
+const deposito = function (valorDepositado) {
+  if (valorDepositado <= 0) {
+    console.log('O valor depositado tem que ser maior que zero.');
+  } else {
+    cliente.saldo += valorDepositado;
+    console.log(cliente.saldo);
+  }
+};
 
-let consultarSaldo = function() {
-    console.log(`Saldo: ${cliente.saldo}`)
-}
+const debito = function (valor) {
+  if (valor > cliente.saldo) {
+    console.log('Saldo insuficiente.');
+  } else {
+    cliente.saldo -= valor;
+    console.log(cliente.saldo);
+  }
+};
 
-let transferencia = function(valorTransferido) {
-    cliente2.saldo -= valorTransferido
-    cliente.saldo += valorTransferido
-    console.log(`Transferido ${valorTransferido} para a conta de ${cliente.nome} com sucesso.`)
-}
+const consultarSaldo = function () {
+  console.log(`Saldo: ${cliente.saldo}`);
+};
 
+const transferencia = function (valorTransferido) {
+  cliente2.saldo -= valorTransferido;
+  cliente.saldo += valorTransferido;
+  console.log(
+    `Transferido ${valorTransferido} para a conta de ${cliente.nome} com sucesso.`
+  );
+};
