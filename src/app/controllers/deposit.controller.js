1c389e2246cdc126ea72590c344bd10a188cf936
bjsec <ayres.sailing@gmail.com>
2021-03-16 10:12:35 -0300

    ref

---
+++ src/app/controllers/deposit.controller.js
@@ -0,0 +1,23 @@
+import validateCPF from '../../helpers/validateCPF.helper';
+import depositDTO from '../models/dto/deposit.dto';
+
+class DepositController {
+  async sotre(req, res) {
+    const schema = depositDTO;
+
+    try {
+      await schema.validate(red.body);
+    } catch (error) {
+      return res.status(400).json({ error_1: error.errors[0] });
+    }
+    const validAccount = await ValidAccount.withAccountValid(
+      req.body.account_id
+    );
+
+    if (!(await validateCPF(req.body.incoming_cpf))) {
+      return res.status(400).json({ error: 'O campo CPF está inválido' });
+    }
+  }
+}
+
+export default new DepositController();
