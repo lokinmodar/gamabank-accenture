bb1990596e18b6b289e96ad9b66e824fe103cd39
bjsec <ayres.sailing@gmail.com>
2021-03-16 22:37:29 -0300

    Criado o modulo de checagem de valor da transação

--- src/app/controllers/deposit.controller.js
+++ src/app/controllers/deposit.controller.js
@@ -10,16 +10,14 @@ class DepositController {
     } catch (error) {
       return res.status(400).json({ error_1: error.errors[0] });
     }
-    /* const validAccount = await ValidAccount.withAccountValid(
+    const validAccount = await ValidAccount.withAccountValid(
       req.body.account_id
-    ); */
+    );
 
     if (!(await validateCPF(req.body.incoming_cpf))) {
       return res.status(400).json({ error: 'O campo CPF está inválido' });
     }
   }
 }
-
-export default new DepositController();
 
 export default new DepositController();
