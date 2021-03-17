2751920aa18864f0084f73d478df912dada45ed4
Dante Souza e Souza <lokinmodar@gmail.com>
2021-03-17 00:05:22 -0300

    Resolvendo conflitos de merge

--- src/app/services/accountBalance.service.js
+++ src/app/services/accountBalance.service.js
@@ -1,15 +1,13 @@
 import Account from '../models/account.model';
 
 module.exports = {
-
-    accountBalance: async (id) => {
-            
-        const accountBalance = await Account.findOne({
-          where: { id }, attributes: [ 'balance' ]
-        }).then (balance => {
-            balance.get('balance')
-        });        
-        return accountBalance        
-    }
-}
-
+  accountBalance: async (id) => {
+    const accountBalance = await Account.findOne({
+      where: { id },
+      attributes: ['balance'],
+    }).then((balance) => {
+      balance.get('balance');
+    });
+    return accountBalance;
+  },
+};
