a3c01e8cd2ac517b2040d9abcc5a46fce08f22df
Diego Augusto <DiegoprogBr@hotmail.com>
2021-03-16 23:47:43 -0300

    criado o controller e dto do internTransfer

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import { Router } from 'express'; // Router gerencia todas as rotas do app
 
+import InternTransferController from './app/controllers/internTransfer.controller';
 import AccountController from './app/controllers/account.controller';
 import SessionController from './app/controllers/session.controller';
 import UserController from './app/controllers/user.controller';
@@ -14,14 +15,13 @@ routes.post('/users', UserController.store); // criação de conta de usuário
 routes.post('/sessions', SessionController.store); // login
 // TODO: criação da lógica para rotas novas
 routes.post('/externaldeposit');
-
+routes.post('/internaltransfer', InternTransferController.store);
 routes.use(authMiddleware); // como está definido aqui, somente rotas que estiverem abaixo dele no código irão usar esse middleware
 
 routes.post('/accounts', AccountController.store); // criação de conta bancária
 
 routes.post('/debitpurchase'); // despesa débito
 routes.post('/carddebit');
-routes.post('/internaltransfer');
 routes.post('/cardpayment');
 
 export default routes;
