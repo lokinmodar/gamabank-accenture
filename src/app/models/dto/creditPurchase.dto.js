55c469501bece8eb4aba79c0c4ac75056d5bcf58
Gabriel Chagas <gabrielserq@outlook.com>
2021-03-17 23:58:08 -0300

    create endpoint credit

--- src/app/models/dto/creditPurchase.dto.js
+++ src/app/models/dto/creditPurchase.dto.js
@@ -3,9 +3,8 @@ import * as Yup from 'yup';
 // validando a requisição
 const creditPurchase = Yup.object()
   .shape({
-    account_id: Yup.number().required(),
     transaction_value: Yup.number().required(),
-        
+    split_in: Yup.number()  
   })
   .label('creditPurchaseRequestDTO');
 
