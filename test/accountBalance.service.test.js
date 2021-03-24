b31ea973c45a2e26e62d2053087bcfc9377ed8fb
Dante Souza e Souza <lokinmodar@gmail.com>
2021-03-24 00:33:33 -0300

    Remodelando testes

--- test/accountBalance.service.test.js
+++ test/accountBalance.service.test.js
@@ -1,15 +1,12 @@
- const chaiHttp = require('chai-http');
-const chai = require('chai');
-const app =require('../src/api/app');
-const {getAccountBalance} = require('../src/api/services/accountBalance.service')
+const {
+  getAccountBalance,
+} = require('../src/api/services/accountBalance.service');
+var should = require('chai').should();
+var expect = require('chai').expect;
 
-
-  var should = require('chai').should()
-  describe('verify balance',function() {
-      it('should return 1000.00',async function() {
-          should.equal('1000.00',await getAccountBalance(1))
-      })
-      it('should return fail',async function() {
-        should.not.equal('1000.00',await getAccountBalance(4))
-    })
-  }) 
+describe('verify balance', function () {
+  it('Expect not null', async function () {
+    let balance = await getAccountBalance(1);
+    expect(balance).to.be.not.null;
+  });
+});
--- test/accountCpf.service.test.js
+++ test/accountCpf.service.test.js
@@ -2,14 +2,14 @@ const chaiHttp = require('chai-http');
 const chai = require('chai');
 const app = require('../src/api/app');
 const {
-  accountWithCpfExists
+  accountWithCpfExists,
 } = require('../src/api/services/accountCpf.service');
 
 let should = require('chai').should();
 
 describe('verify if account exists by cpf', function () {
   it('should return account id if cpf exists', async function () {
-    should.equal(3, await accountWithCpfExists('07306738127'));
+    should.equal(3, await accountWithCpfExists('07306738127')); //TODO: assert error?
   });
   it('should return false if cpf does not exist', async function () {
     should.equal(false, await accountWithCpfExists('10048440426'));
